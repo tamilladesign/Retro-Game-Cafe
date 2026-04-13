b4be0490183b2b5d05446db87fe90dc335ea578b
Tamilla <tamilla.muradova02@gmail.com>
2026-04-13 14:11:01 +0400

    customer spawner

--- Assets/Scripts/CustomerSpawner.cs
+++ Assets/Scripts/CustomerSpawner.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class CustomerSpawner : MonoBehaviour
 {
     public GameObject[] customerPrefabs;
+    private float startDelay = 2f;
+    public float spawnInterval = 5f;
+    private Vector2 spawnPos = new Vector2(-8, -4);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomCustomer", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
+    }
+
+    void SpawnRandomCustomer()
+    {
+        int customerIndex = Random.Range(0, customerPrefabs.Length);
+        Instantiate(customerPrefabs[customerIndex], spawnPos, customerPrefabs[customerIndex].transform.rotation);
     }
 }
