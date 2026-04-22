a3ab4643f226f3e291738779a2462f13739a2b08
srija <srija326@gmail.com>
2026-04-22 14:20:10 -0700

    Some preliminary work on the grid-based navigation.

--- Assets/Scripts/GridMap.cs
+++ Assets/Scripts/GridMap.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Collections;
 using static UnityEngine.GraphicsBuffer;
 
 public class GridMap : MonoBehaviour
@@ -20,6 +22,30 @@ public class GridMap : MonoBehaviour
         snappedPosition.y += grid.cellSize.y / 2.0f;
 
         return snappedPosition;
+    }
+
+    public void UpdateNavInfo()
+    {
+        CustomerDesireable[] allInteractibles = FindObjectsByType<CustomerDesireable>(FindObjectsSortMode.None);
+        for(int i = 0;  i < allInteractibles.Length; i++)
+        {
+            // find bounds of the object
+            Bounds objectBounds = allInteractibles[i].GetComponent<SpriteRenderer>().bounds;
+            for(float y = objectBounds.min.y; y < objectBounds.max.y; y += grid.cellSize.y)
+            {
+                for(float x = objectBounds.min.x; x < objectBounds.max.x; x += grid.cellSize.x)
+                {
+                    Vector3Int currentPoint = grid.WorldToCell(new Vector3(x, y, 0));
+                    // add every point within the bounds of the object
+                    obstacles.Add(new Vector2Int(currentPoint.x, currentPoint.y));
+                }
+            }
+
+            // remove the interaction point for the object
+            Vector2Int interactionPoint = (Vector2Int) grid.WorldToCell(allInteractibles[i].GetInteractionPosition());
+            Vector2Int match = obstacles.Find((a) => { return a.x == interactionPoint.x && a.y == interactionPoint.y; });
+            obstacles.Remove(match);
+        }
     }
 
     public Queue<Vector3> FindPathOnGrid(Vector3 startPos,  Vector3 endPos)
